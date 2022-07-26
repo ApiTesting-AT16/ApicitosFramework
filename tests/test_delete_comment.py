d782f56fbad22f4cac2b6170078c62ebd5798b88
Alex Castro <alexander72577@gmail.com>
2022-07-26 17:12:14 -0400

    Refactor the endpoint of delete comment

--- tests/test_delete_comment.py
+++ tests/test_delete_comment.py
@@ -1,19 +1,18 @@
 import os
-import requests
 from assertpy.assertpy import assert_that
 from pprint import pprint
 from dotenv import load_dotenv
+from crud_comment import CrudComment
 
 load_dotenv()
 URL = os.getenv('BASE_URL')
 TOKEN = os.getenv('ACCESS_TOKEN')
+ID = os.getenv('ID')
 
 
 def test_read_posts():
 
-    headers = {
-        'Authorization': TOKEN
-    }
-    response = requests.delete(URL+'wp-json/wp/v2/comments/6', headers=headers)
-    pprint(response.json())
+    crud_comment = CrudComment()
+    response = crud_comment.delete_comment(URL, TOKEN, ID)
+    # Successfully response
     assert_that(response.status_code).is_equal_to(200)
