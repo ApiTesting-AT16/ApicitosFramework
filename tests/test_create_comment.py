bddda9a8f2e6b5ad172c0de058dc54217c867e4c
abrahamUrquiola <abjin51@gmail.com>
2022-08-01 16:59:16 -0400

    added test cases

--- tests/test_create_comment.py
+++ tests/test_create_comment.py
@@ -4,10 +4,13 @@ from assertpy.assertpy import assert_that
 from pprint import pprint
 from dotenv import load_dotenv
 from crud_comment import CrudComment
+from helpers.login import Login
 
 load_dotenv()
 URL = os.getenv('BASE_URL')
 TOKEN = os.getenv('ACCESS_TOKEN')
+USER = os.getenv('USER')
+PASSWORD = os.getenv('PASSWORD')
 
 
 def test_create_comment():
@@ -18,3 +21,24 @@ def test_create_comment():
     response = crud_users.create_comment(URL, TOKEN, input_data)
     print(response)
     assert_that(response.status_code).is_equal_to(201)
+
+
+def test_create_status():
+
+    #Login().login(USER, PASSWORD)
+    file = open('../testdata/create_comment.json', "r")
+    input_data = json.loads(file.read())
+    crud_comment = CrudComment()
+    response = crud_comment.create_comment(URL, TOKEN, input_data)
+    # See if status change to approved when is filled with invalid data
+    data = json.loads(response.text)
+    assert_that(data["status"]).contains('approved')
+
+def test_create_duplicate_comment():
+
+    file = open('../testdata/create_comment.json', "r")
+    input_data = json.loads(file.read())
+    crud_users = CrudComment()
+    response = crud_users.create_comment(URL, TOKEN, input_data)
+    print(response)
+    assert_that(response.status_code).is_equal_to(409)
--- tests/test_get_list_comments.py
+++ tests/test_get_list_comments.py
@@ -1,9 +1,12 @@
 import os
+import json
+
+import jsonpath
 from assertpy.assertpy import assert_that
-from pprint import pprint
 from dotenv import load_dotenv
 from crud_comment import CrudComment
 from helpers.login import Login
+
 
 load_dotenv()
 URL = os.getenv('BASE_URL')
@@ -20,9 +23,19 @@ def test_get_list_comments():
     assert_that(response.status_code).is_equal_to(200)
 
 
+def test_get_status():
+    crud_comment = CrudComment()
+    response = crud_comment.get_comment(URL, TOKEN)
+    data = json.loads(response.text)
+    print(data)
+    assert_that(str(data[0])).contains('approved')
+
+
 def test_get_invalid_token():
     Login().login(USER, PASSWORD)
     crud_comment = CrudComment()
     response = crud_comment.get_comment(URL, "TOKEN")
     # Error response
     assert_that(response.status_code).is_equal_to(401)
+
+
