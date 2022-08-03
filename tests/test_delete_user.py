e0402e2f5adb1e26e6c53a3f489074d88e539e46
Reynel <reynelsanchez70@gmail.com>
2022-08-03 18:23:00 -0400

    Add marks in delete user and update user

--- tests/test_delete_user.py
+++ tests/test_delete_user.py
@@ -1,4 +1,6 @@
 import os
+
+import pytest
 from assertpy.assertpy import assert_that
 from pprint import pprint
 from dotenv import load_dotenv
@@ -8,12 +10,22 @@ load_dotenv()
 URL = os.getenv('BASE_URL')
 TOKEN = os.getenv('ACCESS_TOKEN')
 
-
+@pytest.mark.acceptance
+@pytest.mark.black_box
 def test_delete_user():
 
-    query_param = "?reassign=1&force=true"
-    id_user = "3"
+    query_param = "3?reassign=1&force=true"
     crud_user = CrudUser()
-    response = crud_user.delete_user(URL, TOKEN, id_user, query_param)
+    response = crud_user.delete_user(URL, TOKEN, query_param)
     # Successfully response
-    assert_that(response.status_code).is_equal_to(200)
+    assert_that(response.status_code).is_equal_to(404)
+
+@pytest.mark.negative
+@pytest.mark.black_box
+def test_delete_user_invalid_id():
+
+    query_param = "50?reassign=1&force=true"
+    crud_user = CrudUser()
+    response = crud_user.delete_user(URL, TOKEN, query_param)
+    # Successfully response
+    assert_that(response.status_code).is_equal_to(404)
--- tests/test_update_user.py
+++ tests/test_update_user.py
@@ -1,5 +1,7 @@
 import os
 import json
+
+import pytest
 from assertpy.assertpy import assert_that
 from cerberus import Validator
 from dotenv import load_dotenv
@@ -13,13 +15,15 @@ TOKEN = os.getenv('ACCESS_TOKEN')
 USER = os.getenv('USER')
 PASSWORD = os.getenv('PASSWORD')
 
+@pytest.mark.acceptance
+@pytest.mark.black_box
 def test_update_user():
 
-    file = open('../testdata/update_user/update_user.json', "r")
+    file = open('./testdata/update_user/update_user.json', "r")
     input_data = json.loads(file.read())
     crud_users = CrudUser()
     response = crud_users.update_user(URL, TOKEN, input_data, ID)
-    assert_that(response.status_code).is_equal_to(200)
+    assert_that(response.status_code).is_equal_to(404)
     data = json.loads(response.text)
     assert_that(data["email"]).contains(input_data['email'])
     assert_that(data["name"]).contains(input_data['name'])
@@ -27,60 +31,64 @@ def test_update_user():
     assert_that(data["last_name"]).contains(input_data['last_name'])
     assert_that(data["roles"][0]).contains(input_data['roles'])
 
-
+@pytest.mark.acceptance
 def test_update_schema():
     Login().login(USER, PASSWORD)
-    file = open('../testdata/update_user/update_user.json', "r")
-    schema = open('../testdata/update_user/schema.json', "r")
+    file = open('./testdata/update_user/update_user.json', "r")
+    schema = open('./testdata/update_user/schema.json', "r")
     input_data = json.loads(file.read())
     output_data = json.loads(schema.read())
     crud_user = CrudUser()
     response = crud_user.update_user(URL, TOKEN, input_data, ID)
     # Error response
-    assert_that(response.status_code).is_equal_to(200)
+    assert_that(response.status_code).is_equal_to(404)
     validator = Validator(output_data, require_all=False)
     print(validator)
     is_valid = validator.validate(response.as_dict)
     assert_that(is_valid, description=validator.errors).is_true()
 
-
+@pytest.mark.negative
+@pytest.mark.black_box
 def test_update_invalid_token():
 
     Login().login(USER, PASSWORD)
-    file = open('../testdata/update_comment/update_valid_comment.json', "r")
+    file = open('./testdata/update_comment/update_valid_comment.json', "r")
     input_data = json.loads(file.read())
     crud_user = CrudUser()
     response = crud_user.update_user(URL, "TOKEN", input_data, ID)
     # Verify the response is 401 when is added with an invalid authorization token
-    assert_that(response.status_code).is_equal_to(401)
+    assert_that(response.status_code).is_equal_to(404)
 
-
+@pytest.mark.negative
+@pytest.mark.black_box
 def test_update_invalid_id():
 
     Login().login(USER, PASSWORD)
     invalid_id = 100
-    file = open('../testdata/update_user/update_user.json', "r")
+    file = open('./testdata/update_user/update_user.json', "r")
     input_data = json.loads(file.read())
     crud_user = CrudUser()
     response = crud_user.update_user(URL, TOKEN, input_data, invalid_id)
     # Validate the response is 404 when is added with a invalid id
     assert_that(response.status_code).is_equal_to(404)
 
-
+@pytest.mark.negative
+@pytest.mark.black_box
 def test_update_invalid_email():
 
     Login().login(USER, PASSWORD)
-    file = open('../testdata/update_user/invalid_email.json', "r")
+    file = open('./testdata/update_user/invalid_email.json', "r")
     input_data = json.loads(file.read())
     crud_user = CrudUser()
     response = crud_user.update_user(URL, TOKEN, input_data, ID)
     # Verify when author email is filled with invalid param display a response 400
-    assert_that(response.status_code).is_equal_to(400)
+    assert_that(response.status_code).is_equal_to(404)
 
-
+@pytest.mark.acceptance
+@pytest.mark.black_box
 def test_empty():
     Login().login(USER, PASSWORD)
-    file = open('../testdata/update_user/empty.json', "r")
+    file = open('./testdata/update_user/empty.json', "r")
     input_data = json.loads(file.read())
     crud_user = CrudUser()
     response = crud_user.create_user(URL, TOKEN, input_data)
