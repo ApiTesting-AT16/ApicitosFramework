d8211e9272cccbc664440405dc3e22124b7ecb99
Alex Castro <alexander72577@gmail.com>
2022-08-03 18:18:51 -0400

    Added marks to all test cases

--- tests/test_create_comment.py
+++ tests/test_create_comment.py
@@ -1,7 +1,7 @@
 import os
 import json
 from assertpy.assertpy import assert_that
-from pprint import pprint
+import pytest
 from dotenv import load_dotenv
 from crud_comment import CrudComment
 
@@ -10,9 +10,10 @@ URL = os.getenv('BASE_URL')
 TOKEN = os.getenv('ACCESS_TOKEN')
 
 
+@pytest.mark.acceptance
 def test_create_comment():
 
-    file = open('../testdata/create_comment.json', "r")
+    file = open('./testdata/create_comment.json', "r")
     input_data = json.loads(file.read())
     crud_users = CrudComment()
     response = crud_users.create_comment(URL, TOKEN, input_data)
--- tests/test_delete_user.py
+++ tests/test_delete_user.py
@@ -1,6 +1,6 @@
 import os
 from assertpy.assertpy import assert_that
-from pprint import pprint
+import pytest
 from dotenv import load_dotenv
 from crud_users import CrudUser
 
@@ -9,11 +9,11 @@ URL = os.getenv('BASE_URL')
 TOKEN = os.getenv('ACCESS_TOKEN')
 
 
+@pytest.mark.acceptance
 def test_delete_user():
 
-    query_param = "?reassign=1&force=true"
-    id_user = "3"
+    query_param = "3?reassign=1&force=true"
     crud_user = CrudUser()
-    response = crud_user.delete_user(URL, TOKEN, id_user, query_param)
+    response = crud_user.delete_user(URL, TOKEN, query_param)
     # Successfully response
     assert_that(response.status_code).is_equal_to(200)
