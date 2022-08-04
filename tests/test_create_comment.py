728e4e725f70c5785dca674683c3c40e83937eee
abrahamUrquiola <abjin51@gmail.com>
2022-08-04 11:51:31 -0400

    add new test cases to get and  post comments

--- tests/test_create_comment.py
+++ tests/test_create_comment.py
@@ -1,5 +1,6 @@
 import os
 import json
+import pytest
 from assertpy.assertpy import assert_that
 from pprint import pprint
 from dotenv import load_dotenv
@@ -13,6 +14,7 @@ USER = os.getenv('USER')
 PASSWORD = os.getenv('PASSWORD')
 
 
+@pytest.mark.acceptance
 def test_create_comment():
 
     file = open('../testdata/create_comment.json', "r")
@@ -23,6 +25,7 @@ def test_create_comment():
     assert_that(response.status_code).is_equal_to(201)
 
 
+@pytest.mark.acceptance
 def test_create_status():
 
     #Login().login(USER, PASSWORD)
@@ -34,11 +37,33 @@ def test_create_status():
     data = json.loads(response.text)
     assert_that(data["status"]).contains('approved')
 
-def test_create_duplicate_comment():
 
+@pytest.mark.negative
+def test_get_invalid_token():
+    #Login().login(USER, PASSWORD)
+    crud_comment = CrudComment()
+    response = crud_comment.get_comment(URL, "TOKEN")
+    # Error response
+    assert_that(response.status_code).is_equal_to(401)
+
+
+@pytest.mark.negative
+def test_create_duplicate_comment():
     file = open('../testdata/create_comment.json', "r")
     input_data = json.loads(file.read())
     crud_users = CrudComment()
     response = crud_users.create_comment(URL, TOKEN, input_data)
     print(response)
     assert_that(response.status_code).is_equal_to(409)
+
+
+@pytest.mark.negative
+def test_update_invalid_email():
+    Login().login(USER, PASSWORD)
+    file = open('../testdata/update_comment/invalid_email.json', "r")
+    input_data = json.loads(file.read())
+    crud_comment = CrudComment()
+    response = crud_comment.create_comment(URL, TOKEN, input_data)
+    # Verify when author email is filled with invalid param display a response 400
+    assert_that(response.status_code).is_equal_to(400)
+
--- tests/test_get_list_comments.py
+++ tests/test_get_list_comments.py
@@ -1,11 +1,12 @@
 import os
 import json
-
+import pytest
 import jsonpath
 from assertpy.assertpy import assert_that
 from dotenv import load_dotenv
 from crud_comment import CrudComment
 from helpers.login import Login
+from utils.schema_validator import validator_schema
 
 
 load_dotenv()
@@ -15,6 +16,7 @@ USER = os.getenv('USER')
 PASSWORD = os.getenv('PASSWORD')
 
 
+@pytest.mark.acceptance
 def test_get_list_comments():
 
     crud_comment = CrudComment()
@@ -23,6 +25,7 @@ def test_get_list_comments():
     assert_that(response.status_code).is_equal_to(200)
 
 
+@pytest.mark.acceptance
 def test_get_status():
     crud_comment = CrudComment()
     response = crud_comment.get_comment(URL, TOKEN)
@@ -31,11 +34,43 @@ def test_get_status():
     assert_that(str(data[0])).contains('approved')
 
 
+@pytest.mark.negative
 def test_get_invalid_token():
-    Login().login(USER, PASSWORD)
+    #Login().login(USER, PASSWORD)
     crud_comment = CrudComment()
     response = crud_comment.get_comment(URL, "TOKEN")
     # Error response
     assert_that(response.status_code).is_equal_to(401)
+
+
+@pytest.mark.negative
+def test_update_invalid_id():
+
+    Login().login(USER, PASSWORD)
+    invalid_id = 100
+    crud_comment = CrudComment()
+    response = crud_comment.get_comment_by_id(URL, TOKEN, invalid_id)
+    # Validate the response is 404 when is added with a invalid id
+    assert_that(response.status_code).is_equal_to(404)
+
+
+@pytest.mark.acceptance
+def test_get_schema():
+    Login().login(USER, PASSWORD)
+    file = open('../testdata/get_comment/get_comment.json', "r")
+    schema = open('../testdata/get_comment/schema.json', "r")
+    input_data = json.loads(file.read())
+    output_data = json.loads(schema.read())
+    crud_comment = CrudComment()
+    response = crud_comment.get_comment(URL, TOKEN, input_data.get("orderby"), input_data.get("page"),
+                                  input_data.get("per_page"))
+    #position = random.randint(0, int(input_data.get("per_page"))-1)
+    position = 1
+    # Error response
+    assert_that(response.status_code).is_equal_to(200)
+    is_valid = validator_schema(output_data, response.as_dict[position])
+    assert_that(is_valid[0], description=is_valid[1].errors).is_true()
+    
+
 
 
